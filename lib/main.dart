f560809b811381332284aed47b58caf04e4102c0
Sardor Ergashaliyev <sardorergashaliyev@gmail.com>
2022-11-08 13:53:34 +0500

    Homework 3

--- lib/main.dart
+++ lib/main.dart
@@ -11,113 +11,59 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Container(
-            color: Colors.black,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  height: 50,
-                  width: 500,
-                  decoration: const BoxDecoration(
-                      color: Color.fromARGB(255, 0, 140, 255),
-                      borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(20),
-                          topRight: Radius.circular(20))),
-                  child: const Center(
-                    child: Text(
-                      'F',
-                      style: TextStyle(
-                          decoration: TextDecoration.none, color: Colors.black),
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                ),
-                Container(
-                  height: 50,
-                  width: 500,
-                  color: Color.fromARGB(255, 24, 182, 255),
-                  child: const Center(
-                    child: Text(
-                      'L',
-                      style: TextStyle(
-                          decoration: TextDecoration.none, color: Colors.black),
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                ),
-                Container(
-                  height: 50,
-                  width: 500,
-                  decoration: const BoxDecoration(
-                    color: Color.fromARGB(255, 64, 194, 255),
-                  ),
-                  child: const Center(
-                    child: Text(
-                      'U',
-                      style: TextStyle(
-                          decoration: TextDecoration.none, color: Colors.black),
-                    ),
-                  ),
-                ),
-                Container(
-                  height: 50,
-                  width: 500,
-                  color: Color.fromARGB(255, 97, 203, 253),
-                  child: const Center(
-                    child: Text(
-                      'T',
-                      style: TextStyle(
-                          decoration: TextDecoration.none, color: Colors.black),
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                ),
-                Container(
-                  height: 50,
-                  width: 500,
-                  color: Color.fromARGB(255, 64, 194, 255),
-                  child: const Center(
-                    child: Text(
-                      'T',
-                      style: TextStyle(
-                          color: Colors.black, decoration: TextDecoration.none),
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                ),
-                Container(
-                  height: 50,
-                  width: 500,
-                  color: Color.fromARGB(255, 24, 182, 255),
-                  child: const Center(
-                    child: Text(
-                      'E',
-                      style: TextStyle(
-                          color: Colors.black, decoration: TextDecoration.none),
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                ),
-                Container(
-                  height: 50,
-                  width: 500,
-                  decoration: const BoxDecoration(
-                      color: Color.fromARGB(255, 0, 140, 255),
-                       borderRadius: BorderRadius.only(
-                        bottomRight: Radius.circular(20),
-                        bottomLeft: Radius.circular(20),
-                      )),
-                
-                  child: const Center(
-                    child: Text(
-                      'R',
-                      style: TextStyle(
-                          color: Colors.black, decoration: TextDecoration.none),
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                )
-              ],
-            )));
+      color: Colors.black,
+      child: Column(
+        children: [
+          Column(
+            children: [
+              Container(
+                height: 150,
+                width: 150,
+                color: Colors.blue,
+              ),
+              Container(
+                height: 150,
+                width: 150,
+                color: Colors.purple,
+              ),
+              Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Container(
+                        height: 150,
+                        width: 150,
+                        color: Colors.green,
+                      ),
+                    ],
+                  )
+                ],
+              ),
+
+              Column(
+                children: [
+                  Container(
+                    height: 150,
+                    width: 150,
+                    color: Colors.white,
+                  )
+                ],
+              ),
+              Container(
+                height: 150,
+                width: 150,
+                color: Colors.red,
+              ),
+              // Container(
+              //   height: 150,
+              //   width: 150,
+              //   color: Colors.yellow,
+              // )
+            ],
+          ),
+        ],
+      ),
+    ));
   }
 }
